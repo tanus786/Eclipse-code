f3309551d6355aba0697e0f51175a5f23fa23aa0
Tanu soni <Tanusoni2312@gmail.com>
2023-07-08 13:38:14 +0530

    Annotation

--- SpringCore/src/main/java/com/springcore/lifecycle/test.java
+++ SpringCore/src/main/java/com/springcore/lifecycle/test.java
@@ -5,11 +5,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class test {
 	public static void main(String[] args) {
 		AbstractApplicationContext con = new ClassPathXmlApplicationContext("com/springcore/lifecycle/lifecycleconfig.xml");
-		Samosa s1 = (Samosa)con.getBean("s1");
-		System.out.println(s1);     
+//		Samosa s1 = (Samosa)con.getBean("s1");
+//		System.out.println(s1);     
 		con.registerShutdownHook();
+//		
+//		Pepsi pep =(Pepsi) con.getBean("pep");
+//		System.out.println(pep);
 		
-		Pepsi pep =(Pepsi) con.getBean("pep");
-		System.out.println(pep);
+		Example ex =(Example) con.getBean("ex");
+		System.out.println(ex);
 	}
 }
